eb5d4fb1174c9332551e777449907d3fe0626cd5
ILYES MIRAOUI <miraouiilyes16@gmail.com>
2024-04-19 13:20:26 +0200

    Partie reservation (sans le bouton )

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use App\Http\Controllers\AppartementController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -13,6 +13,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('appart', AppartementController::class)->except(['index']);
     Route::get('/dashboard', [AppartementController::class, 'userIndex'])->name('dashboard');
 
+    Route::get('/reservation', [ReservationController::class, 'index'])->name('reservation');
 });
 
 Route::get('/', [AppartementController::class, 'index'])->name('appart.index');
